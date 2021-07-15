0481f7cb04f82d2506afcd357f5569c33cb3e77b
eorefujao <emanuel.orefuja@capgemini.com>
2021-07-15 21:17:24 +0100

    removed comment from http redirection

---
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+
+nnamespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        ValuesController controller = new ValuesController();
+        [Fact]
+        public void GetreturnsMyName()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("Emanuel", returnValue.value);
+        }
+
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
+ init
--- test/SimpleAPI.Test/UnitTest1.cs
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -1,10 +1,21 @@
 using System;
 using Xunit;
+using SimpleAPI.Controllers;
+
 
 namespace SimpleAPI.Test
 {
     public class UnitTest1
     {
+        ValuesController controller = new ValuesController();
+        [Fact]
+        public void GetreturnsMyName()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("Emanuel", returnValue.value);
+        }
+
+
         [Fact]
         public void Test1()
         {
